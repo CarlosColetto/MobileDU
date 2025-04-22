effb031f2249459c36a9ee51a1a6be8c3b50750d
Carlos Coletto <carlos.coletto@gmail.com>
2025-04-22 15:54:07 -0300

    Versão com as funcionalidades do app de teste das configurações inseridas

--- app/src/main/java/com/mobile/du/MainActivity.kt
+++ app/src/main/java/com/mobile/du/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.mobile.du
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import utils.TextColorManager
+import com.mobile.libdu.activities.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        ConfigDU.applyFontStyle(this,40f,"#FF4081")
+
+        //ao clicar no icone do DU, abre a Activity de configurações com abas
+        val icone = findViewById<View>(R.id.ic_du)
+        icone.setOnClickListener{val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    // Atualiza a aparência dos textos quando a MainActivity voltar ao foco
+    override fun onResume() {
+        super.onResume()
+        if(TextColorManager.isConfigured) {
+            val rootView = findViewById<View>(android.R.id.content)
+            TextColorManager.updateTextAppearance(rootView)
+        }
     }
 }
