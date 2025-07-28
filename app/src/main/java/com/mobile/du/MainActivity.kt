08dcbdf8acdd111f60f04ec97dae748f84ca7da2
Carlos Coletto <carlos.coletto@gmail.com>
2025-07-28 15:10:33 -0300

    Versão 1.2 funcional

--- app/src/main/java/com/mobile/du/MainActivity.kt
+++ app/src/main/java/com/mobile/du/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.mobile.du
 
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import config.AppConfig
-import utils.LibrasSupportManager
+import utils.VideoPlayerManager
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var videoView: VideoView
     private lateinit var btnPause: Button
     private lateinit var btnResume: Button
     private lateinit var videoName: String
@@ -21,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Garante padding adequado para status bar e gestos
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -28,32 +26,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         AppConfig.load(this)
-
-        videoView = findViewById(R.id.mainVideo)
         videoName = "video_exemplo"
 
         btnPause = findViewById(R.id.btnPauseVideo)
         btnResume = findViewById(R.id.btnResumeVideo)
 
         btnPause.setOnClickListener {
-            if (videoView.isPlaying) videoView.pause()
-            LibrasSupportManager.pauseLibrasVideo()
+            VideoPlayerManager.pause()
         }
 
         btnResume.setOnClickListener {
-            val currentPosition = videoView.currentPosition
-            videoView.setVideoURI(Uri.parse("android.resource://$packageName/raw/$videoName"))
-            videoView.setOnPreparedListener { mp ->
-                mp.seekTo(currentPosition)
-                mp.start()
-            }
-            if (AppConfig.librasEnabled) {
-                LibrasSupportManager.enableLibrasOverlayIfConfigured(this, videoName)
-                LibrasSupportManager.prepareAndPlayLibrasVideo(currentPosition, this)
-            }
+            VideoPlayerManager.resume(this)
         }
 
-        startVideosFromBeginning()
+        VideoPlayerManager.start(this, videoName)
     }
 
     override fun onResume() {
@@ -63,38 +49,9 @@ class MainActivity : AppCompatActivity() {
             com.mobile.mobiledu.EnvironmentApplier.applyToActivity(this)
             com.mobile.mobiledu.SoundApplier.applyToActivity(this)
 
-            startVideosFromBeginning()
-
+            VideoPlayerManager.start(this, videoName)
         } catch (e: Exception) {
             e.printStackTrace()
-        }
-    }
-
-    private fun startVideosFromBeginning() {
-        val videoUri = Uri.parse("android.resource://$packageName/raw/$videoName")
-        videoView.setVideoURI(videoUri)
-        videoView.setOnPreparedListener { mp ->
-            mp.seekTo(0)
-            mp.setOnSeekCompleteListener {
-                mp.start()
-            }
-        }
-        videoView.setOnCompletionListener {
-            LibrasSupportManager.removeLibrasOverlay(this)
-        }
-
-        val librasResId = resources.getIdentifier("${videoName}lb", "raw", packageName)
-        AppConfig.librasVideoURI = if (librasResId != 0) {
-            "android.resource://$packageName/$librasResId"
-        } else {
-            ""
-        }
-
-        if (AppConfig.librasEnabled) {
-            LibrasSupportManager.enableLibrasOverlayIfConfigured(this, videoName)
-            LibrasSupportManager.prepareAndPlayLibrasVideo(0, this)
-        } else {
-            LibrasSupportManager.removeLibrasOverlay(this)
         }
     }
 }
